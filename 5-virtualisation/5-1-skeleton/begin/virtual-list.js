7afbb5d7df930322bfe48eac171dd0d9064dae8f
kamosah <68175563+kamosah@users.noreply.github.com>
2025-06-28 22:33:27 -0400

    chore: add virtualization

--- 5-virtualisation/5-1-skeleton/begin/virtual-list.js
+++ 5-virtualisation/5-1-skeleton/begin/virtual-list.js
@@ -1,34 +1,18 @@
 import {intersectionObserver} from "../../../utils/observer.js";
 
-/**
- * Standard Margin between cards
- * @type {number}
- */
 const MARGIN = 16;
 
-/**
- * Returns top and bottom observer elements
- * @returns {[HTMLElement,HTMLElement]}
- */
 const getObservers = () => [
-    document.getElementById('top-observer'),
-    document.getElementById('bottom-observer')
-]
+  document.getElementById("top-observer"),
+  document.getElementById("bottom-observer"),
+];
 
-/**
- * Returns a virtual list container
- * @returns {HTMLElement}
- */
-function getVirtualList() {
-    return document.getElementById('virtual-list');
+function getList() {
+    return document.getElementById("virtual-list");
 }
 
-/**
- * Returns a main app container
- * @returns {HTMLElement}
- */
 function getContainer() {
-    return document.getElementById('container');
+    return document.getElementById("container");
 }
 
 /**
@@ -37,108 +21,183 @@ function getContainer() {
  *
  * @param element {HTMLElement}
  * @param value {string | number}
- * @returns {?number}
+ * @returns {?string}
  */
 function y(element, value = undefined) {
     if (value != null) {
-        element?.setAttribute('data-y', value);
+        element.setAttribute("data-y", value);
     }
-    const y = element?.getAttribute('data-y');
-    if(y !== '' && y != null && +y === +y) {
-        return +y;
-    }
-    return null;
+    return element.getAttribute("data-y");
 }
 
-/**
- * Returns a CSS Transform Style string to Move Element by certain amount of pixels
- * @param value      - value in pixels
- * @returns {string}
- */
-function translateY(value) {
-    return `translateY(${value}px)`;
-}
-
-/**
- * Starter skeleton
- */
 export class VirtualList {
-    /**
-     * @param root
-     * @param props {{
-     *     getPage: <T>(p: number) => Promise<T[]>,
-     *     getTemplate: <T>(datum: T) => HTMLElement,
-     *     updateTemplate: <T>(datum: T, element: HTMLElement) => HTMLElement,
-     *     pageSize: number
-     * }}
-     */
-    constructor(root, props) {
-        this.props = {...props};
-        this.root = root;
-    }
+  /**
+   * @param root {HTMLElement}
+   * @param props {{
+   *    getPage: <T>(page: number) => Promise<T[]>,
+   *    pageSize: number,
+   *    getTemplate: (...args:any) => HTMLElement,
+   *    updateTemplate: <T>(target: HTMLElement, datum:T) => void,
+   * }}
+   */
+  constructor(root, props) {
+    this.props = { ...props };
+    this.start = 0;
+    this.end = 0;
+    this.root = root;
+    this.pool = [];
+    this.poolLimit = this.props.pageSize * 2;
+  }
 
-    /**
-     * Returns an HTML Representation of the component, should have the following structure:
-     * #container>
-     *    #top-observer+
-     *    #virtual-list+
-     *    #bottom-observer
-     * @returns {string}
-     */
-    toHTML() {
-        /**
-         * Part 1 - App Skeleton
-         *  @todo
-         */
-        return ``.trim();
-    }
+  /**
+   * Returns an HTML Representation of the component, should have the following structure:
+   * #container >
+   *    .top-observer+
+   *    #virtual-list+
+   *    #bottom-observer
+   * @returns {string}
+   */
+  toHTML() {
+    return `<div id="container">
+        <div id="top-observer">Top Observer</div>
+        <main id="virtual-list"></main>
+        <div id="bottom-observer">Bottom Observer</div>
+        </div>`.trim();
+  }
 
-    /**
-     * @returns void
-     */
-    #effect() {
-    }
+  /**
+   * Renders the content to the provided root container and runs
+   * any additional side effects
+   * @returns void
+   */
+  render() {
+    this.root.innerHTML = this.toHTML();
+    this.#effect();
+  }
 
-    /**
-     * @returns void
-     */
-    render() {
-        this.root.innerHTML = this.toHTML();
-        this.#effect()
-    }
-
-    /**
-     * Handles observer intersection entries
-     * @param entries {IntersectionObserverEntry[]}
-     */
-    #handleIntersectionObserver(entries) {}
-
-    async #handleBottomObserver() {}
-
-    async #handleTopObserver() {}
-
-    /**
-     * Function uses `props.getTemplate` to update the html elements
-     * using provided data
-     *
-     * @param elements {HTMLElement[]} - HTML Elements to update
-     * @param data {T[]} - Data to use for update
-     */
-    #updateData(elements, data) {
-
-    }
-
-    /**
-     * Move elements on the screen using CSS Transform
-     *
-     * @param direction {"top" | "down" }
-     */
-    #updateElementsPosition(direction) {
-        const [top, bottom] = getObservers();
-        if (direction === 'down') {
-
-        } else if (direction === 'top') {
-            // To implement
+  /**
+   * Callback implementation for Top and Bottom Intersection observers
+   * @return {IntersectionObserverCallback}
+   */
+  #handleIntersection() {
+    return (entries) => {
+      for (const entry of entries) {
+        if (entry.isIntersecting) {
+          const id = entry.target.id;
+          if (id === "top-observer" && this.start > 0) {
+            void this.#handleTopObserver();
+          } else if (id === "bottom-observer") {
+            void this.#handleBottomObserver();
+          }
         }
+      }
+    };
+  }
+
+  /**
+   * Registers Events / Observers, this function is run after initial render
+   * @returns void
+   */
+  #effect() {
+    intersectionObserver(getObservers(), this.#handleIntersection(), {});
+  }
+
+  /**
+   * Handles Bottom Observer Intersection event
+   * @returns Promise<void>
+   */
+  async #handleBottomObserver() {
+    const container = getContainer();
+    const list = getList();
+    const data = await this.props.getPage(this.end++);
+    const fragment = new DocumentFragment();
+    const elements = data.map((datum) => this.props.getTemplate(datum));
+    elements.forEach((el) => fragment.appendChild(el));
+    if (this.pool.length < this.poolLimit) {
+      this.pool.push(...elements);
+      list.appendChild(fragment);
+    } else {
+      const [unchanged, recycled] = [
+        this.pool.slice(this.props.pageSize, this.pool.length),
+        this.pool.slice(0, this.props.pageSize),
+      ];
+      this.pool = [].concat(unchanged, recycled);
+      this.#updateData(recycled, data);
+      this.start++;
     }
+    this.#updateElement("down");
+    container.style.height = `${container.scrollHeight}px`;
+  }
+
+  /**
+   * Handles Top Observer Intersection event
+   * @returns Promise<void>
+   */
+  async #handleTopObserver() {
+    const data = await this.props.getPage(--this.start);
+    const [recycled, unchanged] = [
+      this.pool.slice(this.props.pageSize, this.pool.length),
+      this.pool.slice(0, this.props.pageSize),
+    ];
+    this.pool = [].concat(recycled, unchanged);
+    this.#updateElement("up");
+    this.#updateData(recycled, data);
+    this.end--;
+  }
+
+  /**
+   * 1. Recycles elements from top or bottom
+   * 2. Updates absolute position of elements and observers
+   *
+   * @param direction {"up" | "down"}
+   * @return {void}
+   */
+  #updateElement(direction) {
+    const [topObserver, bottomObserver] = getObservers();
+    const start = direction === "down" ? 0 : this.props.pageSize - 1;
+    const end = direction === "down" ? this.pool.length : 0;
+    if (direction === "down") {
+      for (let i = start; i < end; i++) {
+        const [prev, current] = [this.pool.at(i - 1), this.pool[i]];
+        if (y(prev) == null) {
+          y(current, "0");
+        } else {
+          const previousY = +y(prev);
+          const previousHeight = prev.getBoundingClientRect().height;
+          const yPos = previousY + (previousHeight + 2 * MARGIN);
+          y(current, yPos);
+          current.style.transform = `translateY(${yPos}px)`;
+        }
+      }
+    } else {
+      for (let i = start; i >= end; i--) {
+        const [prev, current] = [this.pool.at(i + 1), this.pool[i]];
+        if (y(prev) == null) {
+          y(current, "0");
+        } else {
+          const previousY = +y(prev);
+          const previousHeight = prev.getBoundingClientRect().height;
+          const yPos = previousY - (previousHeight + 2 * MARGIN);
+          y(current, yPos);
+          current.style.transform = `translateY(${yPos}px)`;
+        }
+      }
+    }
+    topObserver.style.transform = `translateY(${+y(this.pool[0])}px)`;
+    const lastElement = this.pool.at(-1);
+    const bottomObserverY =
+      +lastElement.getAttribute("data-y") +
+      lastElement.getBoundingClientRect().height +
+      MARGIN * 2;
+    bottomObserver.style.transform = `translateY(${bottomObserverY}px)`;
+  }
+
+  /**
+   * @return {void}
+   */
+  #updateData(elements, data) {
+    for (let i = 0; i < elements.length; i++) {
+      this.props.updateTemplate(elements[i], data[i]);
+    }
+  }
 }
